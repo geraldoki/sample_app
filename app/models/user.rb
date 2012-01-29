0e8d53321f36fccf83038ee9807f82b9ed609291
Gerald Oki <geraldoki@yahoo.com>
2012-01-29 15:03:34 +0100

    Done with Sign-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,13 @@ def self.authenticate(email, submitted_password)
 	
 end
 
+def self.authenticate_with_salt(id, cookie_salt)
+	user = find_by_id(id)
+	(user && user.salt == cookie_salt) ? user : nil
+end
+
+
+
 
 private
  
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, :only =>[:new, :create, :destroy]
 
   match 'signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   
   root :to => 'pages#home'
   
